539e89816161300ef564b06ee077589e50418727
Smotrov Dmitriy <dsxack@gmail.com>
2017-03-26 19:06:31 +0300

    fix

--- main.py
+++ main.py
@@ -25,14 +25,14 @@ def handler(conn, addr):
             break
 
     conn.send("0\r\n\r\n")
-    time.sleep(0.01)
+    time.sleep(0.001)
     conn.close()
 
 
 if __name__ == '__main__':
     sock = socket.socket()
     sock.bind(('0.0.0.0', 8000))
-    sock.listen(0)
+    sock.listen(100)
     print "Start listening 8000 port for connections"
 
     while True:
