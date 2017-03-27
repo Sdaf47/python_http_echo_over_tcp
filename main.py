9351d1a59e311525f96b2624c23e36d1aae86e81
Smotrov Dmitriy <dsxack@gmail.com>
2017-03-28 00:16:25 +0300

    Python3 multiprocessing

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ BUF_SIZE = 512
 
 
 def handler(conn, addr):
-    conn.send("HTTP/1.1 200 OK\n")
-    conn.send("Transfer-Encoding: chunked\n")
-    conn.send("\r\n")
+    conn.send("HTTP/1.1 200 OK\n".encode())
+    conn.send("Transfer-Encoding: chunked\n".encode())
+    conn.send("\r\n".encode())
 
     while True:
         data = conn.recv(BUF_SIZE)
@@ -16,26 +16,28 @@ def handler(conn, addr):
 
         n = len(data)
 
-        conn.send("{0:02x}\r\n".format(n))
+        conn.send("{0:02x}\r\n".format(n).encode())
         conn.send(data)
-        conn.send("\r\n")
+        conn.send("\r\n".encode())
 
         if n < BUF_SIZE:
             break
 
-    conn.send("0\r\n\r\n")
+    conn.send("0\r\n\r\n".encode())
     conn.close()
 
-
 if __name__ == '__main__':
-    sock = socket.socket()
-    sock.bind(('', 8000))
-    sock.listen(100)
-    print("Start listening 8000 port for connections")
-
     pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
 
+    sock = socket.socket()
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', 8000))
+    sock.listen(100)
+
+    print("Start listening 8000 port for connections")
     while True:
         conn, addr = sock.accept()
 
-        pool.Process(target=handler, args=(conn, addr))
+        process = pool.Process(target=handler, args=(conn, addr))
+        process.daemon = True
+        process.start()
